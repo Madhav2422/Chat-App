1c07051709357e366a07ae98bb20f4b7939fd3b9
Madhav Hirani <madhavhirani2422@gmail.com>
2024-10-14 15:48:04 +0530

    mern-chat-app

--- frontend/src/components/Authentication/SignUp.js
+++ frontend/src/components/Authentication/SignUp.js
@@ -1,5 +1,7 @@
 import {React,useState} from 'react'
 import { FormControl, FormLabel, Input, InputGroup, InputRightElement, Show, VStack,Button, Hide, useToast } from '@chakra-ui/react'
+import axios from "axios"
+import{useHistory} from "react-router-dom";
 
 const SignUp = () => {
 
@@ -10,6 +12,7 @@ const SignUp = () => {
     const [pic, setPic] = useState();
     const [show, setShow] = useState(false);
     const [loading,setLoading]=useState(false);
+    const history=useHistory()
     const toast = useToast()
 
 
@@ -31,10 +34,10 @@ const SignUp = () => {
         }
         if(pics.type==="image/jpeg"|| pics.type==="image/png"){
           const data=new FormData();
-          data.append("file",pic);
+          data.append("file",pics);
           data.append("upload_preset","Chat-app")
           data.append("cloud_name","diapribzv")
-          fetch("cloudinary://276472149296782:DMwTscMFx10KbpxOkWKPVjbPz2Q@diapribzv",{
+          fetch("https://api.cloudinary.com/v1_1/diapribzv/image/upload",{
             method:"post",
             body:data,
           }).then((res)=>res.json())
@@ -57,7 +60,62 @@ const SignUp = () => {
           })
         }
     }
-    const submitHandler=()=>{}
+    const submitHandler=async()=>{
+         setLoading(true);
+         if(!name|| !email|| !password||!confirmpassword){
+          toast({
+            title:"Please require all the fiels",
+            status:"warning",
+            duration:5000,
+            isClosable:true,
+            position:"bottom"
+          });
+          setLoading(false);
+         return;
+         }
+
+         if(password!==confirmpassword){
+          toast({
+            title:"Passwords doesnt match",
+            status:"warning",
+            duration:5000,
+            isClosable:true,
+            position:"bottom"
+          });
+         return;
+         }
+
+         try {
+          const config={
+            headers:{
+              "Content-type":"application/json"
+            },
+          };
+          const {data}=await axios.post("api/user/",{name,email,password,pic});
+          toast({
+            title:"Registration Successful",
+            status:"success",
+            duration:5000,
+            isClosable:true,
+            position:"bottom"
+          });
+
+          localStorage.setItem('userInfo',JSON.stringify((data)));
+          setLoading(false);
+          history.push("/chats");
+
+         } catch (error) {
+          toast({
+            title:"Error Occured",
+            description:error.response.data.message,
+            status:"error",
+            duration:5000,
+            isClosable:true,
+            position:"bottom"
+          });
+          setLoading(false);
+         }
+    }
 
    return (
    <VStack spacing="5px">
